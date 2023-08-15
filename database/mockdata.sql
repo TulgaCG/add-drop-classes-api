fe3f66afeb8594c86e2f9cf24009fb61a41126a3
Tulgacan Güdü <tulgacan.gudu@gmail.com>
2023-08-15 16:58:52 +0300

    remove ignored files

--- database/mockdata.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users(id, username, password) 
-VALUES
-(1, "tulgacan", "123456t"),
-(2, "secretuser", "topsecretpass"),
-(3, "peltek", "pel2tek3"),
-(4, "legend27", "72dnegel"),
-(5, "cilgin_delikanli", "hizli1delikanli"),
-(6, "tom334", "luke234");
