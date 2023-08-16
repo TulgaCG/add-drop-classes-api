2866f8ea381a302b8816699d63d73336baa5fc52
Tulgacan Güdü <tulgacan.gudu@gmail.com>
2023-08-16 13:15:37 +0300

    fix typo

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -3,7 +3,6 @@ package server
 import (
 	"github.com/gin-gonic/gin"
 
-	"github.com/TulgaCG/add-drop-classes-api/pkg/auth"
 	"github.com/TulgaCG/add-drop-classes-api/pkg/gendb"
 	"github.com/TulgaCG/add-drop-classes-api/pkg/middleware"
 	"github.com/TulgaCG/add-drop-classes-api/pkg/user"
@@ -15,7 +14,7 @@ func New(db *gendb.Queries) *gin.Engine {
 	r.GET("/users", middleware.DBMiddleware(db), user.Get)
 	r.GET("/users/:id", middleware.DBMiddleware(db), user.GetByID)
 	r.PUT("/users", middleware.DBMiddleware(db), user.Update)
-	r.POST("/users", middleware.DBMiddleware(db), auth.GetByUsername)
+	r.POST("/users", middleware.DBMiddleware(db), user.Post)
 	r.DELETE("/users/:id", middleware.DBMiddleware(db), user.Delete)
 
 	return r
