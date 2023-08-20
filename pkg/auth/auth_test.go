cf5ff2a3d10ca7db1cd385fe16fc141518c5d7a6
Tulgacan Güdü <tulgacan.gudu@gmail.com>
2023-08-20 20:09:31 +0300

    chore: added tests for db operations

---
+++ pkg/auth/auth_test.go
@@ -0,0 +1,116 @@
+package auth
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/TulgaCG/add-drop-classes-api/pkg/database"
+)
+
+func TestCreateRandomToken(t *testing.T) {
+	token, err := createRandomToken()
+	if err != nil {
+		require.NoError(t, err, "createRandomToken failed")
+	}
+
+	require.Len(t, token, tokenLen*2, fmt.Sprintf("expected token size: %d, got: %d", tokenLen*2, len(token)))
+}
+
+func TestGetUserCredentialsWithUsername(t *testing.T) {
+	db, err := database.NewTestDb(context.Background())
+	if err != nil {
+		require.NoError(t, err, "failed to create test db")
+	}
+
+	testCases := []struct {
+		Username    string
+		Password    string
+		ExpectedErr bool
+	}{
+		{"testuser1", "testpassword1", false},
+		{"wronguser", "testpassword2", true},
+	}
+
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("testing getUserCredentialsWithUsername %d", i), func(t *testing.T) {
+			user, err := getUserCredentialsWithUsername(context.Background(), db, testCase.Username)
+			if testCase.ExpectedErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(testCase.Password)); err != nil {
+					require.NoError(t, err)
+				}
+			}
+		})
+	}
+}
+
+func TestLogin(t *testing.T) {
+	db, err := database.NewTestDb(context.Background())
+	if err != nil {
+		require.NoError(t, err, "failed to create test db")
+	}
+
+	testCases := []struct {
+		Username    string
+		Password    string
+		ExpectedErr bool
+	}{
+		{"testuser1", "testpassword1", false},
+		{"testuser2", "wrongpassword", true},
+		{"wronguser", "testpassword3", true},
+	}
+
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("testing login %d", i), func(t *testing.T) {
+			row, err := login(context.Background(), db, testCase.Username, testCase.Password)
+			if testCase.ExpectedErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Len(t, row.Token.String, tokenLen*2)
+				user, err := db.GetUserCredentialsWithUsername(context.Background(), testCase.Username)
+				require.NoError(t, err, "failed to get user by username")
+				require.Equal(t, user.Token.String, row.Token.String)
+			}
+		})
+	}
+}
+
+func TestLogout(t *testing.T) {
+	db, err := database.NewTestDb(context.Background())
+	if err != nil {
+		require.NoError(t, err, "failed to create test db")
+	}
+
+	testCases := []struct {
+		Username    string
+		Password    string
+		ExpectedErr bool
+	}{
+		{"testuser1", "testpassword1", false},
+		{"testuser2", "wrongpassword", false},
+		{"wronguser", "testpassword3", true},
+	}
+
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("testing logout %d", i), func(t *testing.T) {
+			row, _ := login(context.Background(), db, testCase.Username, testCase.Password)
+			err = logout(context.Background(), db, testCase.Username)
+			if testCase.ExpectedErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				user, err := getUserCredentialsWithUsername(context.Background(), db, testCase.Username)
+				require.NoError(t, err, "failed to get user by username")
+				require.Equal(t, user.TokenExpireAt.Valid, false)
+				require.Equal(t, user.Token.String, row.Token.String)
+			}
+		})
+	}
+}
---
+++ pkg/user/user_test.go
@@ -0,0 +1,164 @@
+package user
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/TulgaCG/add-drop-classes-api/pkg/database"
+	"github.com/TulgaCG/add-drop-classes-api/pkg/gendb"
+	"github.com/TulgaCG/add-drop-classes-api/pkg/types"
+)
+
+func TestCreateUser(t *testing.T) {
+	db, err := database.NewTestDb(context.Background())
+	if err != nil {
+		require.NoError(t, err, "failed to create test db")
+	}
+
+	testCases := []struct {
+		Username    string
+		Password    string
+		ExpectedErr bool
+	}{
+		{"testuser1", "testpassword1", true},
+		{"testuser10", "testpassword10", false},
+	}
+
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("testing createUser %d", i), func(t *testing.T) {
+			row, err := createUser(context.Background(), db, testCase.Username, testCase.Password)
+
+			if testCase.ExpectedErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+
+				user, err := getUserByUsername(context.Background(), db, testCase.Username)
+				require.NoError(t, err)
+
+				require.Equal(t, row.ID, user.ID)
+				require.Equal(t, row.Username, user.Username)
+				require.Equal(t, row.Username, testCase.Username)
+			}
+		})
+	}
+}
+
+func TestListUsers(t *testing.T) {
+	db, err := database.NewTestDb(context.Background())
+	require.NoError(t, err, "failed to create test db")
+
+	expectedRows, err := db.ListUsers(context.Background())
+	require.NoError(t, err)
+
+	actualRows, err := listUsers(context.Background(), db)
+	require.NoError(t, err)
+
+	for i := range expectedRows {
+		t.Run(fmt.Sprintf("testing listUsers %d", i), func(t *testing.T) {
+			require.Equal(t, expectedRows[i], actualRows[i], fmt.Sprintf("test failed on row index of %d", i))
+		})
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	db, err := database.NewTestDb(context.Background())
+	require.NoError(t, err, "failed to create test db")
+
+	testCases := []struct {
+		Index       types.UserID
+		ExpectedErr bool
+	}{
+		{0, true},
+		{1, false},
+		{2, false},
+		{3, false},
+		{4, false},
+		{5, false},
+	}
+
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("testing getUser %d", i), func(t *testing.T) {
+			expected, _ := db.GetUser(context.Background(), testCase.Index)
+			actual, err := getUser(context.Background(), db, testCase.Index)
+			if testCase.ExpectedErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, expected, actual)
+			}
+		})
+	}
+}
+
+func TestGetUserByUsername(t *testing.T) {
+	db, err := database.NewTestDb(context.Background())
+	require.NoError(t, err, "failed to create test db")
+
+	testCases := []struct {
+		Username    string
+		ExpectedErr bool
+	}{
+		{"testuser1", false},
+		{"testuser2", false},
+		{"testuser3", false},
+		{"testuser4", false},
+		{"testuser5", false},
+		{"testuser6", true},
+		{"wronguser", true},
+	}
+
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("testing getUserByUsername %d", i), func(t *testing.T) {
+			expected, _ := db.GetUserByUsername(context.Background(), testCase.Username)
+			actual, err := getUserByUsername(context.Background(), db, testCase.Username)
+			if testCase.ExpectedErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, expected, actual)
+			}
+		})
+	}
+}
+
+func TestUpdateUser(t *testing.T) {
+	db, err := database.NewTestDb(context.Background())
+	require.NoError(t, err, "failed to create test db")
+
+	testCases := []struct {
+		Index       types.UserID
+		Username    string
+		Password    string
+		ExpectedErr bool
+	}{
+		{1, "updateduser1", "updatedpassword1", false},
+		{2, "updateduser2", "updatedpassword2", false},
+		{3, "updateduser3", "updatedpassword3", false},
+		{7, "wronguser", "wrongpassword", true},
+	}
+
+	for i, testCase := range testCases {
+		t.Run(fmt.Sprintf("testing updateUser %d", i), func(t *testing.T) {
+			user, _ := db.GetUser(context.Background(), testCase.Index)
+			actual, err := updateUser(context.Background(), db, gendb.UpdateUserParams{
+				ID:       testCase.Index,
+				Username: testCase.Username,
+				Password: testCase.Password,
+			})
+			if testCase.ExpectedErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.NotEqual(t, user, actual)
+
+				expected, _ := db.GetUser(context.Background(), testCase.Index)
+				require.Equal(t, expected.ID, actual.ID)
+				require.Equal(t, expected.Username, actual.Username)
+			}
+		})
+	}
+}
